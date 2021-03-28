45c65e1b30b5243a707a73858e8f320d8e164517
Neith12 <81319608+Neith12@users.noreply.github.com>
2021-03-28 18:24:23 +0200

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -1,3 +1,10 @@
 SELECT nom_recette 
   FROM RECETTES
 
+------------------------------------------
+INSERT INTO `CATEGORIES` ( `ID_CATEGORIE`, `NOM_CATEGORIE` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' )
+			;
